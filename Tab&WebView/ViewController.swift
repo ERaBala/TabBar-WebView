7d210ca4baf6d171927e3f7ae99c7f9a0b54d096
Prasanna S <prasanna@omniprotech.com>
2018-03-06 15:12:14 +0530

    Initial Commit

---
+++ Tab&WebView/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Tab&WebView
+//
+//  Created by OmniproEdge on 06/03/18.
+//  Copyright © 2018 OmniproEdge. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
