5af873934b47e95f627b5428b27d3b0f37276009
OmniproEdge <OmniproEdge_Software_Service_pvt.ltd@OmniProEdges-iMac.local>
2018-03-28 10:44:23 +0530

    first commit

---
+++ Tab&WebView/ViewControllerThree.swift
@@ -0,0 +1,35 @@
+//
+//  ViewControllerThree.swift
+//  Tab&WebView
+//
+//  Created by OmniproEdge on 06/03/18.
+//  Copyright © 2018 OmniproEdge. All rights reserved.
+//
+
+import UIKit
+
+class ViewControllerThree: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
